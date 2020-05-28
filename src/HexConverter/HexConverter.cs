ff5a85e42802930b0267858cb379141f45547a76
Fausto David Suarez Rosario <faustodavid@hotmail.com>
2020-05-28 16:25:20 +0200

    add comments on HexConverter

--- src/HexConverter/HexConverter.cs
+++ src/HexConverter/HexConverter.cs
@@ -4,21 +4,44 @@ using HexConverter.Exceptions;
 
 namespace HexConverter
 {
+    /// <summary>
+    /// High-performance Hexadecimal converter
+    /// </summary>
     public static class HexConverter
     {
+        /// <summary>
+        /// Get the total amount of bytes required. Useful for buffering.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
         public static int GetBytesCount(ReadOnlySpan<char> source)
         {
             return source.Length / 2;
         }
 
+        /// <summary>
+        /// Get bytes from hexadecimal source.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <exception cref="HexConverterInvalidSourceLengthException"></exception>
+        /// <exception cref="HexConverterBufferCapacityException"></exception>
         public static byte[] GetBytes(ReadOnlySpan<char> source)
         {
             byte[] buffer = new byte[GetBytesCount(source)];
             GetBytes(source, buffer);
-            
+
             return buffer;
         }
 
+        /// <summary>
+        /// Get bytes from hexadecimal source. Allowing buffering for high-performance code.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        /// <exception cref="HexConverterInvalidSourceLengthException"></exception>
+        /// <exception cref="HexConverterBufferCapacityException"></exception>
         public static int GetBytes(ReadOnlySpan<char> source, Span<byte> buffer)
         {
             if (source.Length % 2 != 0) throw new HexConverterInvalidSourceLengthException();
@@ -51,26 +74,43 @@ namespace HexConverter
             return requiredBufferSize;
         }
 
+        /// <summary>
+        /// Get bytes from hexadecimal source using ArrayPool to avoid allocations.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns>Bytes on a disposable wrapper to return to array to the pool when dispose</returns>
+        /// <exception cref="HexConverterInvalidSourceLengthException"></exception>
         public static RentedArraySegmentWrapper<byte> GetBytesPooled(ReadOnlySpan<char> source)
         {
-            if(source.Length == 0) throw new HexConverterInvalidSourceLengthException();
-            
+            if (source.Length == 0) throw new HexConverterInvalidSourceLengthException();
+
             ArrayPool<byte> arrayPool = ArrayPool<byte>.Shared;
             byte[] buffer = arrayPool.Rent(GetBytesCount(source));
             var count = GetBytes(source, buffer);
-            
+
             return new RentedArraySegmentWrapper<byte>(new ArraySegment<byte>(buffer, 0, count), arrayPool);
         }
 
+        /// <summary>
+        /// Get the total amount of chars required. Useful for buffering.
+        /// </summary>
+        /// <param name="bytes"></param>
+        /// <returns></returns>
         public static int GetCharsCount(ReadOnlySpan<byte> bytes)
         {
             return bytes.Length * 2;
         }
 
+        /// <summary>
+        /// Get hexadecimal from bytes
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <exception cref="HexConverterInvalidSourceLengthException"></exception>
         public static string GetHex(ReadOnlySpan<byte> source)
         {
-            if(source.Length == 0) throw new HexConverterInvalidSourceLengthException();
-            
+            if (source.Length == 0) throw new HexConverterInvalidSourceLengthException();
+
             ArrayPool<char> arrayPool = ArrayPool<char>.Shared;
             var requiredBufferSize = GetCharsCount(source);
             char[]? disposableBuffer = null;
@@ -89,24 +129,38 @@ namespace HexConverter
                 if (!(disposableBuffer is null)) arrayPool.Return(disposableBuffer);
             }
         }
-
+        
+        /// <summary>
+        /// Get hexadecimal chars from bytes source. Allowing buffering for high-performance code.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        /// <exception cref="HexConverterInvalidSourceLengthException"></exception>
         public static RentedArraySegmentWrapper<char> GetHexPooled(ReadOnlySpan<byte> source)
         {
-            if(source.Length == 0) throw new HexConverterInvalidSourceLengthException();
-            
+            if (source.Length == 0) throw new HexConverterInvalidSourceLengthException();
+
             ArrayPool<char> arrayPool = ArrayPool<char>.Shared;
             var requiredBufferSize = GetCharsCount(source);
             char[] buffer = arrayPool.Rent(requiredBufferSize);
             var count = GetHex(source, buffer);
-            
+
             return new RentedArraySegmentWrapper<char>(new ArraySegment<char>(buffer, 0, count), arrayPool);
         }
 
+        /// <summary>
+        /// Get hexadecimal chars from bytes source using ArrayPool to avoid allocations.
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="buffer"></param>
+        /// <returns>Chars on a disposable wrapper to return to array to the pool when dispose</returns>
+        /// <exception cref="HexConverterInvalidSourceLengthException"></exception>
+        /// <exception cref="HexConverterBufferCapacityException"></exception>
         public static int GetHex(ReadOnlySpan<byte> source, Span<char> buffer)
         {
-            if(source.Length == 0) throw new HexConverterInvalidSourceLengthException();
-            if(buffer.Length < GetCharsCount(source)) throw new HexConverterBufferCapacityException();
-            
+            if (source.Length == 0) throw new HexConverterInvalidSourceLengthException();
+            if (buffer.Length < GetCharsCount(source)) throw new HexConverterBufferCapacityException();
+
             var indexOnBuffer = 0;
 
             for (var indexOnSource = 0; indexOnSource < source.Length; ++indexOnSource, ++indexOnBuffer)
