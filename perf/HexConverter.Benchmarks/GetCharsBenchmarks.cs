5be7dc5576daa611df84a36cbdb7bba470379efd
Fausto David Suarez Rosario <faustodavid@hotmail.com>
2020-05-29 10:23:08 +0200

    Remove loop in perf test

--- perf/HexConverter.Benchmarks/GetCharsBenchmarks.cs
+++ perf/HexConverter.Benchmarks/GetCharsBenchmarks.cs
@@ -11,18 +11,14 @@ namespace HexConverter.Benchmarks
     public class GetCharsBenchmarks
     {
         private byte[] s_fakeHexBytes = Array.Empty<byte>();
-        
-        [Params(4, 20, 60)]
-        public int N { get; set; }
+
+        [Params(4, 20)] public int N { get; set; }
 
         [GlobalSetup]
         public void GlobalSetup()
         {
             StringBuilder sb = new StringBuilder(32 * N);
-            for (int i = 0; i < N; i++)
-            {
-                sb.Append(Guid.NewGuid().ToString());
-            }
+            for (var i = 0; i < N; i++) sb.Append(Guid.NewGuid().ToString());
 
             string text = sb.ToString();
             byte[] expectedTextInBytes = Encoding.UTF8.GetBytes(text);
@@ -47,32 +43,26 @@ namespace HexConverter.Benchmarks
         {
             return BitConverter.ToString(s_fakeHexBytes).Replace("-", "").Length;
         }
-        
+
         [Benchmark]
         public int GetCharsBuffered()
         {
-            int total = 0;
-            for (int i = 0; i < 20; i++)
+            ArrayPool<char> arrayPool = ArrayPool<char>.Shared;
+            var requiredBufferSize = HexConverter.GetCharsCount(s_fakeHexBytes);
+            char[]? disposableBuffer = null;
+            var buffer = requiredBufferSize > 256
+                ? disposableBuffer = arrayPool.Rent(requiredBufferSize)
+                : stackalloc char[requiredBufferSize];
+            try
             {
-                ArrayPool<char> arrayPool = ArrayPool<char>.Shared;
-                int requiredBufferSize = HexConverter.GetCharsCount(s_fakeHexBytes);
-                char[]? disposableBuffer = null;
-                Span<char> buffer = requiredBufferSize > 256
-                    ? (disposableBuffer = arrayPool.Rent(requiredBufferSize))
-                    : stackalloc char[requiredBufferSize];
-                try
-                {
-                    total += HexConverter.GetChars(s_fakeHexBytes, buffer);
-                }
-                finally
-                {
-                    if (!(disposableBuffer is null)) arrayPool.Return(disposableBuffer);
-                }
+                return HexConverter.GetChars(s_fakeHexBytes, buffer);
             }
-
-            return total;
+            finally
+            {
+                if (!(disposableBuffer is null)) arrayPool.Return(disposableBuffer);
+            }
         }
-        
+
         [Benchmark]
         public int GetCharsPooled()
         {
